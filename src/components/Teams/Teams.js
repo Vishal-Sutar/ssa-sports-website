a297e5131cdb00d124b38614f55e2274b0220113
Vishal-Sutar <vishalsutar3731@gmail.com>
2025-05-15 09:43:05 +0530

    first

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -4,7 +4,7 @@ import './Teams.css';
 
 function Teams() {
   const teams = [
-    { id: 1, name: 'Lakers', sport: 'Basketball' },
+    { id: 1, name: 'Lakers', sport: 'Basketball',},
     { id: 2, name: 'Real Madrid', sport: 'Football' },
     { id: 3, name: 'Yankees', sport: 'Baseball' },
     { id: 4, name: 'Warriors', sport: 'Basketball' },
@@ -22,7 +22,8 @@ function Teams() {
             className="team-card"
             style={{ animationDelay: `${index * 0.1}s` }}
           >
-            <div className="team-logo"></div>
+            <div className="team-logo">
+            </div>
             <h3>{team.name}</h3>
             <p>{team.sport}</p>
           </div>
